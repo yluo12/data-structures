7a1cc0f4b6829047888991bcf98c07865dff73de
yluo12 <yantingluo98@gmail.com>
2023-03-29 14:33:00 -0700

    passed tests of functional pattern and functional-shared pattern for stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
+  someInstance.storage = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  // var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // var length = Object.keys(someInstance.storage).length;
+    someInstance.storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var length = someInstance.size();
+    var topValue = someInstance.storage[length - 1];
+    delete someInstance.storage[length - 1];
+    return topValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
 
   return someInstance;
