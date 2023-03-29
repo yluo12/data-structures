4280c5f421ae43dc00c6d2e7c8351736c528f493
Dillon Armstrong <dstrongarm93@gmail.com>
2023-03-29 15:33:33 -0600

    pass all tests for stack shared behavior in stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,31 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    i: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage['key' + someInstance.i] = value;
+    someInstance.i++;
+    //add value to top
   };
 
   someInstance.pop = function() {
+    if (someInstance.i > 0) {
+      someInstance.i -= 1;
+    }
+    var returnValue = storage['key' + someInstance.i];
+    delete storage['key' + someInstance.i];
+    return returnValue;
+    //remove and return value from top
   };
 
   someInstance.size = function() {
+    return someInstance.i;//return numberof items in stack
   };
 
   return someInstance;
