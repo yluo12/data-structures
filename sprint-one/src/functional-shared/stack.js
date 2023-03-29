40b6356cb10466c703fb5f8e1eac3c0783b88000
Dillon Armstrong <dstrongarm93@gmail.com>
2023-03-29 16:54:40 -0600

    pass all tests for function shared/stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = {
+    stackSize: 0
+  };
+  _.extend(instance, stackMethods);
+  return instance;
 };
+var stackTray = {};
 
 var stackMethods = {};
+stackMethods.push = function(value) {
+  stackTray[this.stackSize] = value;
+  this.stackSize++;
+
+  //add value to tray, increase size
+};
+stackMethods.pop = function() {
+  if (this.stackSize > 0) {
+    this.stackSize -= 1;
+  }
+  var topItem = stackTray[this.stackSize];
+  delete stackTray[this.stackSize];
+
+  return topItem;
+  //remove from top and return
+};
+stackMethods.size = function() {
+  return this.stackSize;
+};
 
 
