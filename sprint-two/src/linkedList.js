20ab585e1af03e4befac15d21233e8300e4c940b
Dillon Armstrong <dstrongarm93@gmail.com>
2023-03-30 13:13:40 -0600

    Pass all tests for linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+      return;
+    }
+    var lastNode = list.tail; // find the last node
+    lastNode.next = newNode; // set new node to be the last one in the list
+    list.tail = newNode; // set new tail
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return;
+    }
+    if (list.head === list.tail) {
+      var result = list.head.value;
+      list.head = null;
+      list.tail = null;
+      return result;
+    }
+    var former = list.head.value;
+    list.head = list.head.next;
+    return former;
   };
 
   list.contains = function(target) {
+
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    return false;
+
   };
 
   return list;
@@ -27,3 +60,6 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// linkedList.addToTail: O(1)
+// linkedList.removeHead: O(1)
+// linkedList.contains: O(n)
