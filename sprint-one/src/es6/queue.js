c155eaab9cd33a2d0a3c942f7d8399a9d62e3ec4
yluo12 <yantingluo98@gmail.com>
2023-03-29 17:01:07 -0700

    passed all tests for queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue (value) {
+    this.storage[this.size()] = value;
+  }
+
+  dequeue () {
+    var firstValue = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+      delete this.storage[key];
+    }
+    return firstValue;
+  }
+
+  size () {
+    return Object.keys(this.storage).length;
   }
 
 }
